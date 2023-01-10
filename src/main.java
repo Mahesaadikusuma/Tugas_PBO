b3c756bb1eeaee275830862d6b9c662e6900af85
Mahesaadikusuma <mahesaadikuzuma@gmail.com>
2023-01-10 09:43:17 +0700

    update

--- src/main.java
+++ src/main.java
@@ -9,79 +9,65 @@ public class main {
           do {
                System.out.println("SELAMAT DATANG DI PROGRAM SHOWROOM MOBIL");
                System.out.println("\nMENU\n");
-               System.out.println("1. Category Mobil");
-               System.out.println("2. Jual Beli mobil");
+               System.out.println("1. Jual Mobil");
                System.out.println("0. Keluar");
 
                System.out.print("Masukan Pilihan Anda: ");
                pilihan = InputUser.nextInt();
 
                switch (pilihan) {
+
                     case 1:
-                         Category();
-                         break;
-                    case 2:
                          JualBeli();
                          break;
+
                     default:
                          break;
                }
           } while (pilihan != 0);
 
-     }
-
-     private static void Category() {
-          int pilihan;
-          Scanner InputUser = new Scanner(System.in);
-
-          do {
-               System.out.println("\nMENU Category\n");
-               System.out.println("1. Mobil Tesla");
-               System.out.println("2. Mobil lambo");
-               System.out.println("0. Back To Menu");
-               System.out.print("Masukan Pilihan Anda: ");
-               pilihan = InputUser.nextInt();
-
-               switch (pilihan) {
-                    case 1:
-                         System.out.println("mobil yang anda pilih adalah: " + pilihan + " Mobil Tesla ");
-                         break;
-
-                    case 2:
-                         System.out.println("mobil yang anda pilih adalah: " + pilihan + " Mobil lambo ");
-                         break;
-                    default:
-                         break;
-               }
-          } while (pilihan != 0);
      }
 
      private static void JualBeli() {
-          int pilihan;
-          String mobil;
           Scanner InputUser = new Scanner(System.in);
+          int pilihan, harga, penawaran;
+          String nama, jawaban;
 
+          boolean tawar;
           do {
-               System.out.println("\nMENU Category\n");
+               System.out.println("\n===== JUAL BELI =====\n");
+               System.out.println("MENU");
                System.out.println("1. Jual Mobil");
-               System.out.println("2. Beli Mobil");
-               System.out.println("0. Back To Menu");
                System.out.print("Masukan Pilihan Anda: ");
                pilihan = InputUser.nextInt();
 
                switch (pilihan) {
                     case 1:
-                         System.out.print("mobil apa yang anda ingin jual: ");
-                         mobil = InputUser.next();
-                         System.out.println(mobil);
+                         System.out.print("masukan nama mobil yang ingin dijual: ");
+                         nama = InputUser.next();
+                         System.out.println("nama mobil anda: " + nama);
+
+                         System.out.print("harga mobil yang ingin dijual: ");
+                         harga = InputUser.nextInt();
+                         System.out.print("Harga mobil: Rp " + harga);
+
+                         System.out.println(" Harga mobil yang anda jual terlalu mahal");
+                         System.out.println("mau harga berapa ingin anda tawarkan");
+
+                         System.out.println("Harga yang saya Tawarkan ");
+                         penawaran = InputUser.nextInt();
+
+                         tawar = penawaran <= harga;
+                         jawaban = tawar ? "ya boleh" : "tidak boleh";
+
+                         System.out.println(jawaban);
+
                          break;
 
-                    case 2:
-                         System.out.println("mobil yang anda pilih adalah: " + pilihan + " Mobil lambo ");
-                         break;
                     default:
                          break;
                }
+
           } while (pilihan != 0);
 
      }
